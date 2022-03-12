a4eed43bffc94fab231d54af77928e1d92e6eafe
yexuchen <245849691@qq.com>
2022-03-13 00:52:53 +0800

    完成标签修改和删除功能

---
+++ src/components/GrayBgButton.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const GrayBgButton = styled.button`
+  border: none;
+  background-color: #767676;
+  color: white;
+  padding: 8px 12px;
+  border-radius: 4px;
+`;
+
+export {GrayBgButton}
