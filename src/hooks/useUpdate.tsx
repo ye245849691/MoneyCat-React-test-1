2723a04b4c4dc3dae1601c98dedb35d42c2508f3
yexuchen <245849691@qq.com>
2022-03-13 16:42:31 +0800

    封装useUpdate

---
+++ src/hooks/useUpdate.tsx
@@ -0,0 +1,16 @@
+import {useEffect, useRef} from 'react';
+
+//页面从null到[]会刷新一次，再从[]到[data]还会刷新一次，使用该方法省略前面的一次刷新
+const useUpdate = (fn: () => void, deps: any[]) => {
+  const count = useRef(0);
+  useEffect(() => {
+    count.current += 1;
+  });
+  useEffect(() => {
+    if (count.current > 1) {
+      fn();
+    }
+  }, deps);// eslint-disable-line react-hooks/exhaustive-deps
+};
+
+export {useUpdate};
